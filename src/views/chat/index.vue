b579d2640a432df19095829bcea6d67d9b431f8b
123Fang <38906235+123Fang@users.noreply.github.com>
2024-03-23 21:46:47 +0800

    fix: 重写Chat组件UI

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,297 +1,42 @@
 <script setup lang='ts'>
-import type { Ref } from 'vue'
-import { computed, onMounted, onUnmounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
-import { storeToRefs } from 'pinia'
-import { NAutoComplete, NButton, NInput, useDialog, useMessage } from 'naive-ui'
-import { Message } from './components'
-import { useScroll } from './hooks/useScroll'
-import { useChat } from './hooks/useChat'
-import { HoverButton, SvgIcon } from '@/components/common'
-import { useIsMobile } from '@/hooks/useBasicLayout'
-import { fetchChatAPIProcess } from '@/api'
-import { t } from '@/locales'
-
-let controller = new AbortController()
-
-const openLongReply = import.meta.env.VITE_GLOB_OPEN_LONG_REPLY === 'true'
-
-const route = useRoute()
-const dialog = useDialog()
-const ms = useMessage()
-
-
-const { isMobile } = useIsMobile() // 判断是否移动端
-const { uuid } = route.params as { uuid: string }
-
-const dataSources = computed(() => [])
-const conversationList = computed(() => dataSources.value.filter(item => (!item.inversion && !!item.conversationOptions)))
-
-const prompt = ref<string>('')
-const loading = ref<boolean>(false)
-const inputRef = ref<Ref | null>(null)
-
-// 添加PromptStore
-
-
-
-// 未知原因刷新页面，loading 状态不会重置，手动重置
-dataSources.value.forEach((item, index) => {
-  if (item.loading)
-    updateChatSome(+uuid, index, { loading: false })
-})
-
-function handleSubmit() {
-  // ooo
-  onConversation()
-}
-
-async function onConversation() {
-  // www
-  let message = prompt.value
-
-  if (loading.value)
-    return
-
-  if (!message || message.trim() === '')
-    return
-
-  function addChat() {}
-
-  function fetchChatAPIProcess() {}
-
-  addChat(
-    +uuid,
-    {
-      dateTime: new Date().toLocaleString(),
-      text: message,
-      inversion: true,
-      error: false,
-      conversationOptions: null,
-      requestOptions: { prompt: message, options: null },
-    },
-  )
-
-  loading.value = true
-  prompt.value = ''
-
-  let options: Chat.ConversationRequest = {}
-  const lastContext = conversationList.value[conversationList.value.length - 1]?.conversationOptions
-
-  if (lastContext && usingContext.value)
-    options = { ...lastContext }
-
-  addChat(
-    +uuid,
-    {
-      dateTime: new Date().toLocaleString(),
-      text: t('chat.thinking'),
-      loading: true,
-    },
-  )
-
-  let lastText = ''
-  const fetchChatAPIOnce = async () => {
-    await fetchChatAPIProcess<Chat.ConversationResponse>({
-      prompt: message,
-      options,
-      signal: controller.signal,
-      onDownloadProgress: ({ event }) => {
-        const xhr = event.target
-        const { responseText } = xhr
-        // Always process the final line
-        const lastIndex = responseText.lastIndexOf('\n', responseText.length - 2)
-        let chunk = responseText
-        if (lastIndex !== -1)
-          chunk = responseText.substring(lastIndex)
-        try {
-          const data = JSON.parse(chunk)
-          updateChat(
-            +uuid,
-            dataSources.value.length - 1,
-            {
-              dateTime: new Date().toLocaleString(),
-              text: lastText + (data.text ?? ''),
-              inversion: false,
-              error: false,
-            },
-          )
-
-          if (openLongReply && data.detail.choices[0].finish_reason === 'length') {
-            options.parentMessageId = data.id
-            lastText = data.text
-            message = ''
-            return fetchChatAPIOnce()
-          }
-
-          scrollToBottomIfAtBottom()
-        }
-        catch (error) {
-          //
-        }
-      },
-    })
-    updateChatSome(+uuid, dataSources.value.length - 1, { loading: false })
-  }
-  await fetchChatAPIOnce()
-}
-
-
-
-function handleExport() {
-
-}
-
-function handleDelete(index: number) {
-  if (loading.value)
-    return
-
-  dialog.warning({
-    title: t('chat.deleteMessage'),
-    content: t('chat.deleteMessageConfirm'),
-    positiveText: t('common.yes'),
-    negativeText: t('common.no'),
-    onPositiveClick: () => {
-      chatStore.deleteChatByUuid(+uuid, index)
-    },
-  })
-}
-
-function handleClear() {
-
-}
-
-function handleEnter(event: KeyboardEvent) {
-  // 按 Enter 提交
-}
-
-function handleStop() {
-  if (loading.value) {
-    controller.abort()
-    loading.value = false
-  }
-}
-
-// 可优化部分
-// 搜索选项计算，这里使用value作为索引项，所以当出现重复value时渲染异常(多项同时出现选中效果)
-// 理想状态下其实应该是key作为索引项,但官方的renderOption会出现问题，所以就需要value反renderLabel实现
-const searchOptions = computed(() => {
-  if (prompt.value.startsWith('/')) {
-    return promptTemplate.value.filter((item: { key: string }) => item.key.toLowerCase().includes(prompt.value.substring(1).toLowerCase())).map((obj: { value: any }) => {
-      return {
-        label: obj.value,
-        value: obj.value,
-      }
-    })
-  }
-  else {
-    return []
-  }
-})
-
-// value反渲染key
-const renderOption = (option: { label: string }) => {
-  for (const i of promptTemplate.value) {
-    if (i.value === option.label)
-      return [i.key]
-  }
-  return []
-}
-
-const placeholder = computed(() => {
-  if (isMobile.value)
-    return t('chat.placeholderMobile')
-  return t('chat.placeholder')
-})
-
-const buttonDisabled = computed(() => {
-  return loading.value || !prompt.value || prompt.value.trim() === ''
-})
-
-const footerClass = computed(() => {
-  //  p-4  padding  1ren
-  // p-2  padding  0.5rem
-  let classes = ['p-4']
-  if (isMobile.value)
-    //  pr-3  padding-right 0.75rem
-    classes = ['sticky', 'left-0', 'bottom-0', 'right-0', 'p-2', 'pr-3', 'overflow-hidden']
-  return classes
-})
-
-onMounted(() => {
-  if (inputRef.value && !isMobile.value)
-    inputRef.value?.focus()
-})
-
-onUnmounted(() => {
-  if (loading.value)
-    controller.abort()
-})
+import { NButton, NInput, NScrollbar } from 'naive-ui'
+import Message from './components/Message.vue'
+import { SvgIcon } from '@/components'
 </script>
 
 <template>
-  <div class="flex flex-col w-full h-full">
-    <main class="flex-1 overflow-hidden">
-      <div id="scrollRef" ref="scrollRef" class="h-full overflow-hidden overflow-y-auto">
-        <div id="image-wrapper" class="w-full max-w-screen-xl m-auto dark:bg-[#101014]"
-          :class="[isMobile ? 'p-2' : 'p-4']">
-          <template v-if="!dataSources.length">
-            <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
-              <!--  text-3xl      font-size: 1.875rem; /* 30px */line-height: 2.25rem; /* 36px */-->
-              <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>{{ t('chat.newChatTitle') }}</span>
-            </div>
-          </template>
-          <template v-else>
+  <div class="h-full p-4">
+    <div class="flex flex-col h-full overflow-hidden border rounded-md shadow-md">
+      <header class="flex items-center justify-between p-4">
+        <div>会话</div>
+        <div>&nbsp;</div>
+      </header>
+      <main class="flex-1 overflow-hidden border-y">
+        <div class="h-full">
+          <NScrollbar class="h-full p-4">
             <div>
-              <div>mMssgae</div>
-              <div class="sticky bottom-0 left-0 flex justify-center">
-                <NButton v-if="loading" type="warning" @click="handleStop">
-                  <template #icon>
-                    <SvgIcon icon="ri:stop-circle-line" />
-                  </template>
-                  {{ t('common.stopResponding') }}
-                </NButton>
-              </div>
+              <Message date="10 hours ago" message="hello world" />
+              <Message date="10 hours ago" message="Reprehenderit ut voluptas sapiente ratione nostrum est." user />
+              <Message date="10 hours ago" message="hello world" />
+              <Message date="10 hours ago" message="Reprehenderit ut voluptas sapiente ratione nostrum est." user />
+              <Message date="10 hours ago" message="hello world" />
+              <Message date="10 hours ago" message="Reprehenderit ut voluptas sapiente ratione nostrum est." user />
+              <Message date="10 hours ago" message="hello world" />
+              <Message date="10 hours ago" message="Reprehenderit ut voluptas sapiente ratione nostrum est." user />
+              <Message date="10 hours ago" message="hello world" />
+              <Message date="10 hours ago" message="Reprehenderit ut voluptas sapiente ratione nostrum est." user />
             </div>
-          </template>
+          </NScrollbar>
         </div>
-      </div>
-    </main>
-    <footer :class="footerClass">
-      <div class="w-full max-w-screen-xl m-auto">
+      </main>
+      <footer class="p-4">
         <div class="flex items-center justify-between space-x-2">
-          <HoverButton v-if="!isMobile" @click="handleClear">
-            <span class="text-xl text-[#4f555e] dark:text-white">
-              <SvgIcon icon="ri:delete-bin-line" />
-            </span>
-          </HoverButton>
-          <HoverButton v-if="!isMobile" @click="handleExport">
-            <span class="text-xl text-[#4f555e] dark:text-white">
-              <SvgIcon icon="ri:download-2-line" />
-            </span>
-          </HoverButton>
-          <HoverButton>
-            <span class="text-xl" :class="{ 'text-[#4b9e5f]': true }">
-              <SvgIcon icon="ri:chat-history-line" />
-            </span>
-          </HoverButton>
-          <NAutoComplete v-model:value="prompt" :options="searchOptions" :render-label="renderOption">
-            <template #default="{ handleInput, handleBlur, handleFocus }">
-              <NInput ref="inputRef" v-model:value="prompt" type="textarea" :placeholder="placeholder"
-                :autosize="{ minRows: 1, maxRows: isMobile ? 4 : 8 }" @input="handleInput" @focus="handleFocus"
-                @blur="handleBlur" @keypress="handleEnter" />
-            </template>
-          </NAutoComplete>
-          <NButton type="primary" :disabled="buttonDisabled" @click="handleSubmit">
-            <template #icon>
-              <span class="dark:text-black">
-                <SvgIcon icon="ri:send-plane-fill" />
-              </span>
-            </template>
+          <NInput placeholder="Type a message" />
+          <NButton type="primary">
+            <SvgIcon icon="ri:send-plane-fill" />
           </NButton>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   </div>
 </template>
