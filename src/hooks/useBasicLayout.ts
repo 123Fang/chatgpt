d78b93d50508d0d71b125207d0fba1a4676ccbd1
123Fang <38906235+123Fang@users.noreply.github.com>
2024-03-25 09:58:07 +0800

    fix: Chat组件

--- src/hooks/useBasicLayout.ts
+++ src/hooks/useBasicLayout.ts
@@ -1,9 +1,7 @@
 import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 
-export function useIsMobile() {
-  // breakpointsTailwind 是用于定义 Tailwind CSS 的断点（breakpoints）的对象，包含了不同屏幕尺寸的断点信息
+export function useBasicLayout() {
   const breakpoints = useBreakpoints(breakpointsTailwind)
-  // 当前屏幕尺寸是否符合 'sm'（小屏幕）的断点条件
   const isMobile = breakpoints.smaller('sm')
 
   return { isMobile }
--- src/views/chat/layout/Layout.vue
+++ src/views/chat/layout/Layout.vue
@@ -1,24 +1,26 @@
 <script setup lang='ts'>
 import { computed } from 'vue'
-import { NLayout, NLayoutContent,NLayoutSider } from 'naive-ui'
+import { NLayout, NLayoutContent } from 'naive-ui'
 import { useRouter } from 'vue-router'
 import Sider from './sider/index.vue'
-import { useIsMobile } from '@/hooks/useBasicLayout'
-import { useAppStore } from '@/store'
+import Header from './header/index.vue'
+import { useBasicLayout } from '@/hooks/useBasicLayout'
+import { useAppStore, useChatStore } from '@/store'
 
 const router = useRouter()
 const appStore = useAppStore()
+const chatStore = useChatStore()
 
+router.replace({ name: 'Chat', params: { uuid: chatStore.active } })
 
-const { isMobile } = useIsMobile()
+const { isMobile } = useBasicLayout()
 
 const collapsed = computed(() => appStore.siderCollapsed)
-
 
 const getMobileClass = computed(() => {
   if (isMobile.value)
     return ['rounded-none', 'shadow-none']
-  return ['border', 'rounded-md', 'shadow-md', 'dark:border-neutral-800']
+  return ['border', 'rounded-md', 'shadow-md']
 })
 
 const getContainerClass = computed(() => {
@@ -30,16 +32,17 @@ const getContainerClass = computed(() => {
 </script>
 
 <template>
-  <div class="h-full dark:bg-[#24272e] transition-all" :class="[isMobile ? 'p-0' : 'p-4']">
+  <div class="h-full" :class="[isMobile ? 'p-0' : 'p-4']">
     <div class="h-full overflow-hidden" :class="getMobileClass">
-      <n-layout class="z-40 transition" :class="getContainerClass" has-sider>
-        <Sider/>
+      <NLayout class="z-40 transition" :class="getContainerClass" has-sider>
+        <Sider />
+        <Header v-if="isMobile" />
         <NLayoutContent class="h-full">
           <RouterView v-slot="{ Component, route }">
             <component :is="Component" :key="route.fullPath" />
           </RouterView>
         </NLayoutContent>
-      </n-layout>
+      </NLayout>
     </div>
   </div>
 </template>
